7c7f978e1741f4e4a072aa73488de486bbbfa6d3
Quincy <quincythai50@icloud.com>
2023-07-13 21:57:28 -0700

    Move all declarations to top

--- main.js
+++ main.js
@@ -1,3 +1,86 @@
+const MAX_NUMBERS = 16;
+let displayValue;
+let firstValue;
+let operator = null;
+let timeToResetDisplay = false;
+
+const screen = document.getElementById('screen-text');
+const subtextParagraph = document.getElementById('subtext');
+
+const clearButton = document.getElementById('clear-button');
+const numberButtons = document.querySelectorAll('.number-button');
+const operationButtons = document.querySelectorAll('.operator-button');
+const equalsButton = document.getElementById('equal-button');
+
+clearButton.addEventListener('click', clearScreen);
+numberButtons.forEach((button) => {
+  button.addEventListener('click', () => populate(button.textContent));
+});
+operationButtons.forEach((button) => {
+  button.addEventListener('click', () => setOperator(button.textContent));
+});
+equalsButton.addEventListener('click', evaluate);
+
+function clearScreen() {
+  screen.textContent = "0";
+  firstValue = 0;
+  displayValue = 0;
+  operator = null;
+  timeToResetDisplay = false;
+  subtextParagraph.textContent = "";
+}
+
+function populate(number) {
+  if (screen.textContent.length < MAX_NUMBERS || timeToResetDisplay) {
+    if (screen.textContent === "0" || timeToResetDisplay) {
+      screen.textContent = number;
+      timeToResetDisplay = false;
+    } else {
+      screen.textContent += number;
+    }
+    displayValue = Number(screen.textContent);
+    if (operator != null) {
+      subtextParagraph.textContent = `${firstValue} ${operator} ${displayValue}`;
+    }
+  }
+}
+
+function setOperator(op) {
+  firstValue = displayValue;
+  operator = op;
+  timeToResetDisplay = true;
+  subtextParagraph.textContent = `${firstValue} ${operator}`;
+}
+
+function evaluate() {
+  if (firstValue != null && operator != null) {
+    let result = operate(operator, firstValue, displayValue);
+    updateDisplay(result);
+    subtextParagraph.textContent = `${firstValue} ${operator} ${displayValue} = `;
+  }
+}
+
+function updateDisplay(result) {
+  if (result === null) {
+    screen.style.fontSize = "26px";
+    if (operator === '%') {
+      screen.textContent = "Error, cannot % by 0.";
+    } else if (operator === '÷') {
+      screen.textContent = "Error, cannot ÷ by 0.";
+    } else {
+      screen.textContent = "Error";
+    }
+    timeToResetDisplay = true; 
+  } else {
+    screen.textContent = round(result);
+  }
+}
+
+function round(value) {
+  return parseFloat(value.toFixed(3))
+}
+
+// Functions for addition, subtraction, multiplication, division, and modulo
 function add(a, b) {
   return a + b;
 }
@@ -37,88 +120,4 @@ function operate(operator, a, b) {
       alert("Error");
       return null;
   }
-}
-
-const clearButton = document.getElementById('clear-button');
-clearButton.addEventListener('click', clearScreen);
-
-function clearScreen() {
-  screen.textContent = "0";
-  firstValue = 0;
-  displayValue = 0;
-  operator = null;
-  timeToResetDisplay = false;
-  subtextParagraph.textContent = "";
-}
-
-const MAX_NUMBERS = 16;
-let displayValue;
-const numberButtons = document.querySelectorAll('.number-button');
-numberButtons.forEach((button) => {
-  button.addEventListener('click', () => populate(button.textContent));
-});
-
-const screen = document.getElementById('screen-text');
-function populate(number) {
-  if (screen.textContent.length < MAX_NUMBERS || timeToResetDisplay) {
-    if (screen.textContent === "0" || timeToResetDisplay) {
-      screen.textContent = number;
-      timeToResetDisplay = false;
-    } else {
-      screen.textContent += number;
-    }
-    displayValue = Number(screen.textContent);
-    if (operator != null) {
-      subtextParagraph.textContent = `${firstValue} ${operator} ${displayValue}`;
-    }
-  }
-}
-
-const operationButtons = document.querySelectorAll('.operator-button');
-operationButtons.forEach((button) => {
-  button.addEventListener('click', () => setOperator(button.textContent));
-})
-
-let firstValue;
-let operator = null;
-let timeToResetDisplay = false;
-function setOperator(op) {
-  firstValue = displayValue;
-  operator = op;
-  console.log(operator);
-  timeToResetDisplay = true;
-  subtextParagraph.textContent = `${firstValue} ${operator}`;
-}
-
-const equalsButton = document.getElementById('equal-button');
-equalsButton.addEventListener('click', evaluate);
-
-function evaluate() {
-  if (firstValue != null && operator != null) {
-    let result = operate(operator, firstValue, displayValue);
-    updateDisplay(result);
-    subtextParagraph.textContent = `${firstValue} ${operator} ${displayValue} = `;
-  }
-}
-
-function updateDisplay(result) {
-  if (result === null) {
-    screen.style.fontSize = "26px";
-    if (operator === '%') {
-      screen.textContent = "Error, cannot % by 0.";
-    } else if (operator === '÷') {
-      screen.textContent = "Error, cannot ÷ by 0.";
-    } else {
-      screen.textContent = "Error";
-    }
-    timeToResetDisplay = true; 
-  } else {
-    screen.textContent = round(result);
-  }
-}
-
-const subtextParagraph = document.getElementById('subtext');
-
-function round(value) {
-  return parseFloat(value.toFixed(3))
 }
