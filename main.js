b5b974d33afe76bfc78b509ca185cad729ac9977
Quincy <quincythai50@icloud.com>
2023-07-13 18:43:16 -0700

    Complete basic functions

--- main.js
+++ main.js
@@ -11,20 +11,11 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  if (b === 0) {
-    errorByZeroDivison = true;
-    return;
-  } else {
-    return a / b;
-  }
+  return a / b;
 }
 
 function modulo(a, b) {
-  if (b === 0) {
-    errorByZeroDivison = true;
-  } else {
-    return a % b;
-  }
+  return a % b;
 }
 
 // Returns the result of using a operator b.
@@ -41,106 +32,7 @@ function operate(operator, a, b) {
     case '%':
       return modulo(a, b);
     default:
-      console.log("Error");
-      break;
+      alert("Error");
+      return null;
   }
-}
-
-// allows number buttons to display on calculator screen
-let displayValue = 0;
-let firstValue = 0;
-let secondValue = 0;
-let result = 0;
-let operator = null;
-let pendingOperation = false;
-let errorByZeroDivison = false;
-
-const display = document.getElementById('screen-text');
-const numberButtons = document.querySelectorAll(".number-button");
-
-// can use empty argument and arrow function to avoid passing e to function
-// i.e. JS preserves references to the button object
-numberButtons.forEach(button => {
-  button.addEventListener('click', () => populate(button.innerHTML));
-})
-
-function populate(number) {
-  if (displayValue === 0) {
-    display.innerHTML = "";
-  } 
-
-  if (pendingOperation) {
-    display.innerHTML = number;
-  } else {
-    display.innerHTML += number;
-  }
-  displayValue = parseInt(display.innerHTML);
-}
-
-const operatorButtons = document.querySelectorAll(".operator-button");
-operatorButtons.forEach(button => {
-  button.addEventListener('click', () => setOperator(button.innerHTML));
-});
-
-function setOperator(op) {
-  if (pendingOperation) {
-    calculate();
-  } else {
-    operator = op;
-    firstValue = displayValue;
-    pendingOperation = true;
-    displaySubtext();
-  }
-}
-
-const clearButton = document.getElementById('clear-button');
-clearButton.addEventListener('click', clearDisplay);
-function clearDisplay() {
-  displayValue = 0;
-  display.innerHTML = "0";
-  subtextParagraph.innerHTML = "";
-  reset();
-}
-
-// Rounds value to 3 decimals
-function displayResult(value) {
-  // First get decimal (EX: 0.500), then remove trailing 0's
-  if (errorByZeroDivison) {
-    display.style.fontSize = "30px";
-    display.innerHTML = "Error, divison by 0";
-  } else {
-    display.innerHTML = parseFloat(value.toFixed(3));
-  }
-}
-
-const subtextParagraph = document.getElementById('subtext');
-function displaySubtext() {
-  if (operator != null) {
-    subtextParagraph.innerHTML = `${firstValue} ${operator}`
-  }
-  if (secondValue == null) {
-    subtextParagraph.innerHTML += "= ";
-  }
-}
-
-const equalButton = document.getElementById('equal-button');
-equalButton.addEventListener('click', calculate)
-
-function calculate() {
-  debugger
-  if (pendingOperation) {
-    secondValue = displayValue;
-    result = operate(operator, firstValue, secondValue);
-    displayResult(result);
-  }
-}
-
-
-function reset() {
-  displayValue = 0;
-  firstValue = 0;
-  secondValue = 0;
-  operator = null;
-  pendingOperation = false;
-  errorByZeroDivison = false;
 }
